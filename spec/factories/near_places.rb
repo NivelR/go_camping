6d65c3a611f7abd02b302a788040582d20f950c3
Matias M <matiasmasca@gmail.com>
2015-12-08 11:08:46 -0300

    Faker: lo puse como comentario para poder hacer el deploy local que se trababa con un error.

--- spec/factories/near_places.rb
+++ spec/factories/near_places.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
   factory :near_place do
     kiosk false
-disco false
-beach false
-national_park false
-other "MyString"
-place nil
+    disco false
+    beach false
+    national_park false
+    other "MyString"
+    place nil
   end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name Faker::Name.name
-    email Faker::Internet.email
+    name "Pepe Argento" #Faker::Name.name
+    email "pepe@argento.com.ar" #Faker::Internet.email
     password Devise.friendly_token.first(8)
   end
-
 end
